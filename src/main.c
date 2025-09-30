80c2f92489db3b31f91916daeb3c1ebb8286a367
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-09-30 10:34:16 +0700

    Complete testing for rosenbrock

--- src/main.c
+++ src/main.c
@@ -10,18 +10,26 @@ int main()
 {
 	printf("Static Rosenbrock Test\n");
 
+
+	RosenbrockFirstCase();
+	RosenbrockSecondCase();
+	RosenbrockThirdCase();
+	
 	return 0;
 }
 
 void RosenbrockFirstCase()
 {
 	printf("Rosenbrock test case 1: x = 1, y =1\n");
+	printf("Result = %lf\n", Rosenbrock(1, 1));
 }
 void RosenbrockSecondCase()
 {
 	printf("Rosenbrock test case 2: x = 2, y =2\n");
+	printf("Result = %lf\n", Rosenbrock(2, 2));
 }
 void RosenbrockThirdCase()
 {
 	printf("Rosenbrock test case 3: x = 3, y =3\n");
+	printf("Result = %lf\n", Rosenbrock(3, 3));
 }
