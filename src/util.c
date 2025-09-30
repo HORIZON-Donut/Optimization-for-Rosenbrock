6c6b0145a2a320764be87d9e12260b835e21a4be
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-09-30 16:28:47 +0700

    Add helper function to set point

--- src/util.c
+++ src/util.c
@@ -2,7 +2,14 @@
 #include "util.h"
 
 #include <stdio.h>
+#include <math.h>
 
+static void setPoint(Point* point, double x, double y, double f)
+{
+	point-> x = x;
+	point-> y = y;
+	point-> f = f;
+}
 void printPoint(Point point)
 {
 	printf("----------\nX = %lf\nY = %lf\nF = %lf\n----------\n", point.x, point.y, point.f);
