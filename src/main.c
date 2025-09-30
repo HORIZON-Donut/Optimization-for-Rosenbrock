06194395c0cc71e3debbd0552985e20c795c1330
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-09-30 10:31:28 +0700

    Prepare stuff for testing rosenbrock stuff

--- src/main.c
+++ src/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
+#include "rosenbrock.h"
+
+void RosenbrockFirstCase();
+void RosenbrockSecondCase();
+void RosenbrockThirdCase();
+
 int main()
 {
-	printf("Hello. World!\n");
+	printf("Static Rosenbrock Test\n");
 
 	return 0;
+}
+
+void RosenbrockFirstCase()
+{
+	printf("Rosenbrock test case 1: x = 1, y =1\n");
+}
+void RosenbrockSecondCase()
+{
+	printf("Rosenbrock test case 2: x = 2, y =2\n");
+}
+void RosenbrockThirdCase()
+{
+	printf("Rosenbrock test case 3: x = 3, y =3\n");
 }
