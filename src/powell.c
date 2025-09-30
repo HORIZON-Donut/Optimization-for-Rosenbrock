329355fcc5f46b6eb057a807449c824a022306a2
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-09-30 12:14:57 +0700

    Add stuff to test line search function -------------------------------------- line search function are not exposed. So, it need other exposed function to test it

--- src/powell.c
+++ src/powell.c
@@ -1,7 +1,14 @@
 
 #include "powell.h"
 
+#include "rosenbrock.h"
+
 static double line_search(double (*f)(double, double), double x, double y)
 {
 	return f(x, y);
+}
+
+void PowellMethod(Point* point)
+{
+	point->f = line_search(Rosenbrock, point->x, point->y);
 }
