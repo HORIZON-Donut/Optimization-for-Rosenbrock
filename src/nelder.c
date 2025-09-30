d71cbaea345b6577d80a6efc392d3ca1d66efa2c
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-09-30 18:08:38 +0700

    Move blueprint to other location

--- src/nelder.c
+++ src/nelder.c
@@ -2,6 +2,20 @@
 #include "nelder.h"
 
 
+def dual_sort(key, data):
+    """Sort `data` based on values in `key` (bubble sort style)."""
+    n = len(key)
+    swap = True
+
+    while swap:
+        swap = False
+        for i in range(n - 1):
+            if key[i] > key[i+1]:
+                key[i], key[i+1] = key[i+1], key[i]
+                data[i], data[i+1] = data[i+1], data[i]
+                swap = True
+    return data, key
+
 def Reflection(Xc, Xw, alpha=1.0):
     """Reflection step."""
     return [Xc[i] + alpha*(Xc[i] - Xw[i]) for i in range(len(Xc))]
--- src/util.c
+++ src/util.c
@@ -15,17 +15,3 @@ void InitialPoint(Point* point, double x, double y)
 
 	point->f = 0;
 }
-
-def dual_sort(key, data):
-    """Sort `data` based on values in `key` (bubble sort style)."""
-    n = len(key)
-    swap = True
-
-    while swap:
-        swap = False
-        for i in range(n - 1):
-            if key[i] > key[i+1]:
-                key[i], key[i+1] = key[i+1], key[i]
-                data[i], data[i+1] = data[i+1], data[i]
-                swap = True
-    return data, key
