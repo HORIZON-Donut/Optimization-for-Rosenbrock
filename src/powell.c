bcaedf0a59aad4112bc89c0257102a758691f169
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-09-30 13:12:04 +0700

    Complete line search function

--- src/powell.c
+++ src/powell.c
@@ -13,7 +13,27 @@ static double line_search(double (*f)(double, double), double x, double y, doubl
 	double gr = (sqrt(5.0) - 1.0) / 2.0;
 	double c = rb - gr * (rb - lb);
 	double d = lb - gr * (rb - lb);
-	return f(x, y);
+
+	while (fabs(c - d) > 1.e-5)
+	{
+		double fc = f(x + c * dx, y + c * dy);
+		double fd = f(x + d * dx, y + d * dy);
+
+		if (fc < fd)
+		{
+			rb = d;
+			d = c;
+			d = rb - gr * (rb - lb);
+		}
+
+		else
+		{
+			lb = c;
+			c = d;
+			d = lb + gr * ( rb - lb);
+		}
+	}
+	return (lb + rb) / 2.0;
 }
 
 void PowellMethod(Point* point)
