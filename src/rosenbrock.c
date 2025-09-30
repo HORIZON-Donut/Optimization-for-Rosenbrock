6a0da469fcdd08d1173b6bc7c05533863ae9b938
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-09-30 10:27:16 +0700

    Add rosenbrock stuff into their file

---
+++ src/rosenbrock.c
@@ -0,0 +1,7 @@
+
+#include "rosenbrock.h"
+
+double Rosenbrock(double x, double y)
+{
+	return (1 - x) * (1 - x) + 100 * (y - x * x) * (y - x * x)
+}
---
+++ src/rosenbrock.h
@@ -0,0 +1,7 @@
+
+#ifndef INCLUDE_ROSENBROCK_H
+#define INCLUDE_ROSENBROCK_H
+
+double Rosenbrock(double x, double y);
+
+#endif
