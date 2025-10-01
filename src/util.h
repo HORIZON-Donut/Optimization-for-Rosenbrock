7ab512b7a0c7feb4bcc169b0c129ad58dbfe9a60
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-10-02 06:37:19 +0700

    Remove vertices

--- src/util.h
+++ src/util.h
@@ -12,13 +12,6 @@ typedef struct Point
 	double f;
 }Point;
 
-typedef struct Vertice
-{
-	double x;
-    double y;
-    double z;
-}Vertice;
-
 void InitialPoint(Point* point, double x, double y);
 void printPoint(Point point);
 void sortPoint(Point* point, int num);
