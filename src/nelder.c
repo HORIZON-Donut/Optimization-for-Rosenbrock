a9bb6204546d2384cde01fceb5003da1532b83c1
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-10-02 07:33:45 +0700

    Add expansion function

--- src/nelder.c
+++ src/nelder.c
@@ -30,7 +30,12 @@ def Expansion(Xc, Xr, mseXr, x, y, gamma=2.0):
     mseXe = MSE(Xe, x, y)
     return (Xe if mseXe < mseXr else Xr), min(mseXe, mseXr)
 */
-
+static void expansion(Point xc, Point xr, Point* xe)
+{
+	xe->x = xc.x + GAMMA * (xc.x - xr.x);
+	xe->y = xc.y + GAMMA * (xc.y - xr.y);
+	xe->f = Rosenbrock(xe.x, xe.y);
+}
 /*
 def Outside_Cont(Xc, Xr, x, y, beta=0.5):
     """Outside contraction."""
